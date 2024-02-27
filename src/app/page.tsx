56e7262958dc4575e31c01cd7abe57f721f87d26
Sergiioog <sergiogargor9@gmail.com>
2024-02-27 22:07:16 +0100

    offCanvas terminado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Header from './ui/Header';
 
+
 export default function Home() {
   return (
     <main className="min-h-screen ">
       <Header />
+      <section className="fotoPrincipal min-h-[60vh]">
+        <p>Hola</p>
+      </section>
     </main>
   );
 }
