15229368c036ae7853aecf0058d2a354f7f2304b
Sergiioog <sergiogargor9@gmail.com>
2024-02-28 00:52:01 +0100

    Carrusel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import Header from './ui/Header';
-
+import {ImagesSlider} from './ui/image-slider';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="min-h-screen ">
+    <>
+    <main className="min-h-screen">
       <Header />
       <section className="fotoPrincipal min-h-[60vh]">
-        <p>Hola</p>
+        <div className='relative max-h-96 h-96 w-full'>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full flex flex-col items-center justify-center">
+          <h1 className="text-6xl font-semibold mb-[13px]">MyAgenda</h1>
+          <h2 className="text-3xl font-semibold">Tu app deportiva favorita</h2>
+        </div>
+          <ImagesSlider
+            images={[
+              "/assets/fotos/carrusel1.jpeg",
+              "/assets/fotos/carrusel2.jpeg",
+              "/assets/fotos/carrusel3.jpeg"
+            ]}
+            autoplay={true}
+            direction='up'
+          />
+        </div>
+
+
       </section>
-    </main>
+    </main></>
   );
 }
