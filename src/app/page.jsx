3fff376f46d793ee83b2a11a167e1284506fdd84
Sergiioog <sergiogargor9@gmail.com>
2024-02-28 16:59:04 +0100

    Boton saber mas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -30,18 +30,22 @@ export default function Home() {
       <section className="min-h-[60vh] flex justify-start items-center flex-col mt-20 max-w-[90%] ml-auto mr-auto">
         <h3 className='h3Titulo  text-white animacionAparecer text-center'>¿Que es MyAgenda?</h3>
         <div className=' w-full h-auto'>
-          <p className='pFont text-white mt-5 tracking-tight text-center'>MyAgenda es una aplicación deportiva, pensada para que cualquier usuario de gimnsio
+          <p className='pFont text-white mt-6 tracking-tight text-center'>MyAgenda es una aplicación deportiva, pensada para que cualquier usuario de gimnsio
             pueda registrar su progreso con el fin de ganar masa muscular
           </p>
-          <p className='pFont text-white mt-5 tracking-tight text-center'>Con nuestro novedoso calendario podrás registrar
+          <p className='pFont text-white mt-8 tracking-tight text-center'>Con nuestro novedoso calendario podrás registrar
           tu actividad y ver tu progeso gracias a nuestro apartado de estadísticas.
           </p>
-          <div className=' h-auto w-full flex justify-center items-center mt-5'>
+          <div className=' h-auto w-full flex justify-center items-center mt-8'>
 
             <Boton texto={"Saber más"} 
-                  ancho={100} 
-                  alto={200} 
-                  colorTexto='white'   />
+                  ancho={220} 
+                  alto={50} 
+                  borderRadius={10}
+                  colorTexto='white'
+                  colorFondo='black'
+                  colorBorde='white'  
+            />
           
           </div>
 
--- src/app/ui/Button.tsx
+++ src/app/ui/Button.tsx
@@ -1,4 +1,4 @@
-import { color } from 'framer-motion';
+
 import React from 'react';
 
 const Boton = (props: { 
@@ -7,19 +7,29 @@ const Boton = (props: {
     alto:number;
     borderRadius:number;
     colorTexto:string; 
+    colorFondo:string;
+    colorBorde:string;
+
 
 }) => {
-    const {texto,ancho,alto,borderRadius,colorTexto} = props;
+    const {texto,ancho,alto,borderRadius,colorTexto,colorFondo,colorBorde} = props;
 
     const estilosBoton = {
         width: `${ancho}px`,
-        alto: `${alto}px`,
+        height: `${alto}px`,
         borderRadius: `${borderRadius}px`,
         color: `${colorTexto}`,
+        backgroundColor : `${colorFondo}`,
+        border : `${colorBorde}`,
     }
 
     return(
-        <button style={estilosBoton}>{texto}</button>
+        <button style={estilosBoton} className='flex justify-center items-center'>
+            <p className='font-semibold text-xl tracking-tight mr-3'>{texto}</p>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+            </svg>
+        </button>
     )
 }
 
