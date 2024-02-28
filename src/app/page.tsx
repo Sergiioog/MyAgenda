766e84b5e5b451e7e3a9746c9b7aef55b50be38b
Sergiioog <sergiogargor9@gmail.com>
2024-02-28 01:01:59 +0100

    Arreglos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
     <main className="min-h-screen">
       <Header />
       <section className="fotoPrincipal min-h-[60vh]">
-        <div className='relative max-h-96 h-96 w-full'>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full flex flex-col items-center justify-center">
-          <h1 className="text-6xl font-semibold mb-[13px]">MyAgenda</h1>
-          <h2 className="text-3xl font-semibold">Tu app deportiva favorita</h2>
+        <div className='relative max-h-[55vh] h-[55vh] w-full'>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full flex flex-col items-center justify-center p-5">
+          <h1 className="text-5xl font-semibold mb-[13px] text-center">MyAgenda</h1>
+          <h2 className="text-3xl font-semibold text-center">Tu app deportiva favorita</h2>
         </div>
           <ImagesSlider
             images={[
